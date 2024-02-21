f9186cb409a6ee75ac9c9cb4350c51dbf55ec727
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-22 00:32:11 +0530

    Styled Components: Pseudo Selectors, Nested Rules & Media Queries

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -6,7 +6,7 @@ const ControlContainer = styled.div`
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
 const Label = styled.label`
   display: block;
   margin-bottom: 0.5rem;
@@ -15,7 +15,7 @@ const Label = styled.label`
   letter-spacing: 0.1em;
   text-transform: uppercase;
   color:  ${({$invalid}) => $invalid ? '#f87171' : '#6b7280'};
-`
+`;
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
@@ -25,7 +25,22 @@ const Input = styled.input`
   border: 1px solid  ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
+
+const Button = styled.button`
+      padding: 1rem 2rem;
+      font-weight: 600;
+      text-transform: uppercase;
+      border-radius: 0.25rem;
+      color: #1f2937;
+      background-color: #f0b322;
+      border-radius: 6px;
+      border: none;
+
+      &:hover {
+        background-color: #f0920e;
+      }
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -73,7 +88,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );

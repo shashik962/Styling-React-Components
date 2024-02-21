af3152e8b2f8e25cdfec469686cb424454dab6ef
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-21 23:30:59 +0530

    Scoping CSS Rules with CSS Modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import logo from '../assets/logo.png';
-import './Header.css';
+import classess from './Header.module.css';
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-        color: 'red',
-        textAlign: 'left'
-      }}>A community of artists and art-lovers.</p>
+      <p className={classess.paragraph}>A community of artists and art-lovers.</p>
     </header>
   );
 }

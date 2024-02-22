9fa4fbc8c9464f5c0d4d9cd089ab8f081c486e45
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-02-22 21:32:07 +0530

    Migrating The Demo App to Tailwind CSS

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
 import Button from './Button.jsx';
 import Input from './Input.jsx';
-
-const ControlContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
 
 
 export default function AuthInputs() {
@@ -33,8 +24,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         {/* Split or reuse components */}
         <Input
             label="Email"
@@ -51,9 +42,9 @@ export default function AuthInputs() {
             }
           />
       
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
